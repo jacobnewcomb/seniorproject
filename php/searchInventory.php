a7cb0ea33226acdbd26b0339eec425a3944fd8e6
TUdby <74376246+TUdby@users.noreply.github.com>
2023-02-21 22:14:26 -0800

    Inventory search

---
+++ php/searchInventory.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'databaseConnection.php';
+$instance = ConnectDb::getInstance();
+$conn = $instance->getConnection();
+
+$item = $_POST['item'];
+$query = "SELECT * FROM inventory WHERE name LIKE '%" . $item . "%';";
+
+$results = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($results) > 0) :
+    foreach ($results as $items) : ?>
+        <tr>
+            <td><button style="width: 100%; height: 1em; background-color: green"></td>
+            <td style="text-align: center"><?= $items["name"]; ?></td>
+            <td style="text-align: center">$<?= $items["price_per_unit"]; ?></td>
+            <td><button>+</button></td>
+            <td style="text-align: center"><?= $items["quantity"]; ?></td>
+            <td><button>-</button></td>
+            <td><button style="width: 100%; height: 1em; background-color: red"></button></td>
+        </tr>
+    <?php endforeach;
+else : ?>
+    <tr>
+        <td colspan="5">No record found...</td>
+    </tr>
+<?php endif; ?>
