ee5ff5ba3662cd5ffc1564bb32529493e980e473
jacobnewcomb <jacobnewc@gmail.com>
2023-03-19 13:46:11 -0700

    moreeeeee

--- php/commonQueryFunctions.php
+++ php/commonQueryFunctions.php
@@ -101,7 +101,7 @@
         $invoice_id = mysqli_fetch_assoc($exec)["invoice_id"];
         $query = "INSERT INTO `appointments`(`cust_car_id`, `invoice_id`, `start_date`, `end_date`, `location`, `notes`) VALUES (" . $cust_car_id . ", " . $invoice_id . ", '" . $start_date . "', '" . $end_date . "', '" . $location . "', '" . $notes . "')";
         $exec = mysqli_query($conn, $query);
-        $query = "SELECT apt_id FROM appointments WHERE invoice_id = " . $invoice_id . " AND extended_apt IS NULL";
+        $query = "SELECT apt_id FROM appointments WHERE invoice_id = " . $invoice_id;
         $exec = mysqli_query($conn, $query);
         $apt_id = mysqli_fetch_assoc($exec)["apt_id"];
         $query = "UPDATE invoices SET root_apt_id = " . $apt_id . " WHERE invoice_id = " . $invoice_id;
