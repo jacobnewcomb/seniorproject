76de9886ef911f6f03fa1b1dbb67c5f82f69bbc5
TUdby <74376246+TUdby@users.noreply.github.com>
2023-03-19 08:31:40 -0700

    add item name and invoice button link to accounting

--- php/accountingBackend.php
+++ php/accountingBackend.php
@@ -10,24 +10,42 @@ $ledger = mysqli_query($conn, $query);
 foreach($ledger as $entry):
 
     $expense = $entry['quantity'] * $entry['expense_per_unit'];
+
     $item_id = $entry['item_id'];
+    if($item_id != 0) {
+        $query = "SELECT name FROM inventory WHERE item_id='$item_id';";
+        $item_name = mysqli_fetch_assoc(mysqli_query($conn, $query))['name'];
+    } else {
+        $item_name = "";
+    }
+
     $invoice_id = $entry['invoice_id'];
     if($entry['withdraw'] == 0):
 ?>
-    <tr>
-        <td style="color: green;"><?= $expense?></td>
-        <td></td>
+        <tr>
+            <td style="color: green;"><?= $expense?></td>
+            <td></td>
 <?php
     else:
 ?>
-    <tr>
-        <td></td>
-        <td style="color: red;">-<?= $expense?></td>
+        <tr>
+            <td></td>
+            <td style="color: red;">-<?= $expense?></td>
 <?php
     endif;
 ?>
-        <td><?= $item_id != 0 ? $item_id : ""?></td>
-        <td><?= $invoice_id != 0 ? $invoice_id : ""?></td>
+            <td><?= $item_name?></td>
+<?php
+    if ($invoice_id != 0) :
+?>
+        <td><button onclick="window.location.href=location.protocol+'//'+location.host+'/html/invoice.html?invoice_id=' + <?= $invoice_id ?>"><?= $invoice_id ?></button></td>
+<?php
+    else:
+?>
+        <td></td>
+<?php
+    endif;
+?>
         <td><?= $entry['note']?></td>
     </tr>
 <?php
